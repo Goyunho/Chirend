b2cde2bf477dd2dcbc2a97862b2a3831e6ce38f5
Goyunho <rhdbsgh0629@naver.com>
2016-10-22 11:39:40 +0900

    161022

--- app/src/main/java/com/example/rhdbs/chirend/ShowTypeRst.java
+++ app/src/main/java/com/example/rhdbs/chirend/ShowTypeRst.java
@@ -1,28 +1,16 @@
 package com.example.rhdbs.chirend;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-
-import java.util.ArrayList;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by rhdbs on 2016-10-13.
  */
 
-public class ShowTypeRst extends Activity{
+public class ShowTypeRst extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_showtyperst);
-
-        // 차트 데이터
-        ArrayList<String> labels = new ArrayList<String>();
-        labels.add("January");
-        labels.add("February");
-        labels.add("March");
-        labels.add("April");
-        labels.add("May");
-        labels.add("June");
-
+        setTitle("6세, 남자아이");
     }
 }
--- app/src/main/java/com/example/rhdbs/chirend/SubActivity.java
+++ app/src/main/java/com/example/rhdbs/chirend/SubActivity.java
@@ -3,11 +3,8 @@ package com.example.rhdbs.chirend;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.NumberPicker;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 /**
@@ -23,21 +20,27 @@ public class SubActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sub);
         setTitle("설문조사");
 
-
         age_picker = (NumberPicker) findViewById(R.id.age);
         age_picker.setMinValue(0);
         age_picker.setMaxValue(20);
 
         gender_RG = (RadioGroup) findViewById(R.id.gender);
-
+/*
+        Handler hd2 = new Handler();
+        hd2.postDelayed(new SubActivity.RstType(), 5000); // 3초 후에 hd Handler 실행
+*/
+    }
+    private class RstType implements Runnable{
+        public void run() {
+            startActivity(new Intent(getApplication(), ShowTypeRst.class)); // 로딩이 끝난후 이동할 Activity
+        }
     }
 
     public void showRst(View v){
         Intent intent = new Intent(this, ShowTypeRst.class);
-        intent.putExtra("age", age_picker.getValue());
+        /*intent.putExtra("age", age_picker.getValue());
         RadioButton rb = (RadioButton) findViewById(gender_RG.getCheckedRadioButtonId());
-        intent.putExtra("gender", rb.getText().toString());
+        intent.putExtra("gender", rb.getText().toString());*/
         startActivity(intent);
     }
-
 }
