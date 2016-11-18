41be1f0d39216446d7c700dd74a2f8446c739bc8
Goyunho <rhdbsgh0629@naver.com>
2016-11-18 09:47:15 +0900

    ㄹㅇ 끄읕

--- app/src/main/java/com/example/rhdbs/chirend/LoadingActivity.java
+++ app/src/main/java/com/example/rhdbs/chirend/LoadingActivity.java
@@ -6,9 +6,13 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoadingActivity extends AppCompatActivity {
     Button next;
+    TextView tv_server_check;
+    String str_server_check;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +20,22 @@ public class LoadingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_loading);
 
         next = (Button) findViewById(R.id.next);
+        tv_server_check = (TextView) findViewById(R.id.tv_server_check);
+
+        str_server_check = MakedModulesByGoLim.getData("http://naneg93.dothome.co.kr/?check=1");
+        //Toast.makeText(getApplication(), str_server_check, Toast.LENGTH_SHORT).show();
 
         Handler hd = new Handler();
         hd.postDelayed(new splashhandler(), 1000); // 3초 후에 hd Handler 실행
 
+
     }
     private class splashhandler implements Runnable{
         public void run() {
-            next.setVisibility(android.view.View.VISIBLE);
+            if(str_server_check.equals("error getData"))
+                tv_server_check.setVisibility(View.VISIBLE);
+            else
+                next.setVisibility(android.view.View.VISIBLE);
         }
     }
 
